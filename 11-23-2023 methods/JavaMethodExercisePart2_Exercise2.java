934fb32a0f9253dc41cb26a54330ca21f0f37c0c
Mohan Dong <117117710+firefly-yrdsb@users.noreply.github.com>
2023-11-24 15:07:21 +0000

    Squashed commit of the following:
    commit 77c81918dae0f196ee10b47ce6d9b44ce485d97d
    Author: Mohan Dong <117117710+firefly-yrdsb@users.noreply.github.com>
    Date:   Fri Nov 24 15:04:08 2023 +0000
    
        Add javadoc docs to methods

--- 11-23-2023 methods/JavaMethodExercisePart2_Exercise2.java
+++ 11-23-2023 methods/JavaMethodExercisePart2_Exercise2.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class JavaMethodExercisePart2_Exercise2 {
+    /**
+     * main method
+     * This procedural method prompts the user to enter a count and then calls the
+     * print method that many times using a for loop.
+     * 
+     * @param args - unused
+     * 
+     * @return void
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,10 +22,17 @@ public class JavaMethodExercisePart2_Exercise2 {
             print();
     }
 
+    /**
+     * print method
+     * This procedural method prints 10 '+' characters and a line feed at the end to
+     * the console by using a for loop.
+     * 
+     * @return void
+     */
     private static void print() {
         for (int i = 0; i < 10; ++i)
             System.out.print('+');
 
         System.out.println();
-    }
+    } // print()
 }
--- 11-23-2023 methods/JavaMethodExercisePart2_Exercise3.java
+++ 11-23-2023 methods/JavaMethodExercisePart2_Exercise3.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class JavaMethodExercisePart2_Exercise3 {
+    /**
+     * main method
+     * This procedural method prompts the user to enter a length and width and then
+     * calls the print method using those inputs.
+     * 
+     * @param args - unused
+     * 
+     * @return void
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -15,6 +24,17 @@ public class JavaMethodExercisePart2_Exercise3 {
         print(length, width);
     }
 
+    /**
+     * print method
+     * This procedural method prints a filled rectangle of '&' characters to the
+     * console with a line feed at the end of
+     * each line using for loops.
+     * 
+     * @param length - the length (horizontal) of the rectangle
+     * @param width  - the width (vertical) of the rectangle
+     * 
+     * @return void
+     */
     private static void print(int length, int width) {
         for (int i = 0; i < width; ++i) {
             for (int j = 0; j < length; ++j)
