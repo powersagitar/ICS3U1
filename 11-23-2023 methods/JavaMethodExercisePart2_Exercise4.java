c43508a4058449402efe51a90401774dd11c9727
Mohan Dong <117117710+firefly-yrdsb@users.noreply.github.com>
2023-11-27 15:35:03 +0000

    Squashed commit of the following:
    commit d35944ca9ecf8e59923fa389b0f3e345e36b9224
    Author: Mohan Dong <117117710+firefly-yrdsb@users.noreply.github.com>
    Date:   Mon Nov 27 15:34:34 2023 +0000
    
        Add javadoc to all methods

--- 11-23-2023 methods/JavaMethodExercisePart2_Exercise4.java
+++ 11-23-2023 methods/JavaMethodExercisePart2_Exercise4.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class JavaMethodExercisePart2_Exercise4 {
+    /**
+     * Main method to print a rectangle with the given length, width, and symbol.
+     * 
+     * @param args - command line arguments
+     * 
+     * @return void
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -18,6 +25,15 @@ public class JavaMethodExercisePart2_Exercise4 {
         print(length, width, symbol);
     }
 
+    /**
+     * Prints a rectangle with the given length, width, and symbol.
+     * 
+     * @param length - length (horizontal) of the rectangle
+     * @param width  - width (vertical) of the rectangle
+     * @param symbol - symbol to print the rectangle with
+     * 
+     * @return void
+     */
     private static void print(int length, int width, char symbol) {
         for (int i = 0; i < width; ++i) {
             for (int j = 0; j < length; ++j) {
--- 11-23-2023 methods/JavaMethodExercisePart2_Exercise5.java
+++ 11-23-2023 methods/JavaMethodExercisePart2_Exercise5.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class JavaMethodExercisePart2_Exercise5 {
+    /**
+     * Main method to print a right triangle with the given side length and symbol.
+     * 
+     * @param args - command line arguments
+     * 
+     * @return void
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -15,6 +22,14 @@ public class JavaMethodExercisePart2_Exercise5 {
         print(side, symbol);
     }
 
+    /**
+     * Prints a right triangle with the given side length and symbol.
+     * 
+     * @param side   - side length of the triangle
+     * @param symbol - symbol to print the triangle with
+     * 
+     * @return void
+     */
     private static void print(int side, char symbol) {
         for (int i = 0; i < side; ++i) {
             for (int j = 0; j <= i; ++j)
