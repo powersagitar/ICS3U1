6e8bfb0a4462eae209a9ced8fecd8af27faadcb9
Mohan Dong <117117710+firefly-yrdsb@users.noreply.github.com>
2023-11-30 15:26:46 +0000

    Squashed commit of the following:
    commit 3ed6196b2e5997f253647f187dd022414a6bd245
    Author: Mohan Dong <117117710+firefly-yrdsb@users.noreply.github.com>
    Date:   Thu Nov 30 15:26:19 2023 +0000
    
        Add javadoc comments to source file
    
    commit 67b8a2088c25d4f7ef056bae2f3ed1c6d3f4ba08
    Author: Mohan Dong <117117710+firefly-yrdsb@users.noreply.github.com>
    Date:   Thu Nov 30 15:15:53 2023 +0000
    
        Add try catch blocks to exercise 6

--- 11-23-2023 methods/JavaMethodExercisePart2_Exercise6.java
+++ 11-23-2023 methods/JavaMethodExercisePart2_Exercise6.java
@@ -1,26 +1,46 @@
-import java.util.Scanner;
+import java.io.*;
 import java.text.DecimalFormat;
 
 public class JavaMethodExercisePart2_Exercise6 {
-    static final int NUMBER_COUNT = 3;
+    static final private int NUMBER_COUNT = 3;
 
+    /**
+     * Get 3 inputs, calculate sum, average and highest number, then print them.
+     * 
+     * @param args is unused.
+     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int numbers[] = new int[NUMBER_COUNT];
 
-        for (int i = 0; i < NUMBER_COUNT; ++i) {
+        for (int i = 0; i < NUMBER_COUNT;) {
             System.out.println("Enter an integer number:");
-            numbers[i] = scanner.nextInt();
-        }
 
-        scanner.close();
+            try {
+                numbers[i] = Integer.parseInt(bufferedReader.readLine());
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+                continue;
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid number format: " + ex.getMessage());
+                continue;
+            }
+
+            ++i;
+        }
 
         System.out.println("Sum: " + getSum(numbers));
         System.out.println("Average: " + getAverageStr(numbers));
         System.out.println("Highest: " + getHighest(numbers));
     }
 
+    /**
+     * Get sum of numbers.
+     * 
+     * @param numbers is an array of numbers.
+     * @return sum of numbers.
+     */
     private static int getSum(int numbers[]) {
         int sum = 0;
 
@@ -30,12 +50,24 @@ public class JavaMethodExercisePart2_Exercise6 {
         return sum;
     }
 
+    /**
+     * Get average of numbers.
+     * 
+     * @param numbers is an array of numbers.
+     * @return average of numbers.
+     */
     private static StringBuffer getAverageStr(int numbers[]) {
         double average = (double) getSum(numbers) / (double) NUMBER_COUNT;
 
         return new StringBuffer(new DecimalFormat(".00").format(average));
     }
 
+    /**
+     * Get highest number.
+     * 
+     * @param numbers is an array of numbers.
+     * @return highest number.
+     */
     private static int getHighest(int numbers[]) {
         int max = numbers[0];
 
